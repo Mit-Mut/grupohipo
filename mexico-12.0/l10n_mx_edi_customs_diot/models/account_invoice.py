b3885a77a1f8afb9237a2261862b2c41041da19b
Magpemr12 <guadalupemendoza@mit-mut.com>
2019-09-02 16:45:43 -0500

    instala modulo de localización avanzada

---
+++ mexico-12.0/l10n_mx_edi_customs_diot/models/account_invoice.py
@@ -0,0 +1,26 @@
+# Copyright 2018 Vauxoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models, fields
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    l10n_mx_edi_customs_id = fields.Many2one(
+        'l10n_mx_edi.customs', 'Custom',
+        help='Custom in which this invoice income in the company.')
+    l10n_mx_edi_customs_extra_id = fields.Many2one(
+        'l10n_mx_edi.customs', 'Customs Extra',
+        help='Custom in which this invoice income in the company.')
+    l10n_mx_edi_customs_total = fields.Monetary(
+        'Customs Total', compute='_compute_customs_total',
+        currency_field='company_currency_id',
+        help='On invoices with customs saves the amount total in the company '
+        'currency based in the customs rate.')
+
+    @api.depends('l10n_mx_edi_customs_id.rate', 'amount_total')
+    def _compute_customs_total(self):
+        for record in self.filtered('l10n_mx_edi_customs_id'):
+            record.l10n_mx_edi_customs_total = (
+                record.amount_total * record.l10n_mx_edi_customs_id.rate)
